938b276d3025883239c956a5c19c9808439d1638
wolfiee42 <saifalislam2022@gmail.com>
2024-05-28 07:57:05 +0600

    understanding gsap in basic level and along the way practicing it too

--- Basics/script.js
+++ Basics/script.js
@@ -7,3 +7,21 @@ gsap.to("#box1", {
   backgroundColor: "yellow",
   scale: 0.5,
 });
+
+gsap.from("h1", {
+  y: 40,
+  opacity: 0,
+  delay: 1,
+  duration: 0.5,
+  stagger: 1,
+});
+
+gsap.to("#box", {
+  x: 1200,
+  duration: 2,
+  delay: 1,
+  rotate: 260,
+  repeat: -1,
+  yoyo: true,
+  borderRadius: "50%",
+});
